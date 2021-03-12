b5acc2bf6423ef36552689328a5e6904bdf41a17
Fabricio Izumi <fabricio.izumi@indt.org.br>
2021-03-12 09:08:56 -0400

    Initial commit

---
+++ app2.js
@@ -0,0 +1,22 @@
+express=require('express');
+
+app=express();
+
+app.get("/", (req, res) => {
+	res.json({item: {key: 'value'}});
+});
+
+app.get("/dtv/current-service", (req, res) => {
+	res.json(
+		{
+			contentType: "0x10",
+			streamType: "0x03",
+			languageCode: "por"
+		}
+	);
+});
+
+
+app.listen(4000, () => {
+	console.log("hello");
+});
