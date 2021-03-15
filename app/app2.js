d232fa12a9c80a33a393aae12f03c753c098e4f5
Fabricio Izumi <fabricio.izumi@indt.org.br>
2021-03-15 16:29:23 -0400

    Included video component service

--- app/app2.js
+++ app/app2.js
@@ -31,6 +31,36 @@ app.get("/dtv/current-service/apps", (req, res) => {
 	);
 });
 
+app.get("/dtv/current-service/0x00", (req, res) => {
+	res.json(
+		{
+			streamContent: "0x6",
+			componentType: "0x03",
+			componentTag: "0x00",
+			streamType: "0x01",
+			ISO639languageCode: "por",
+			pid: 123,
+			state: "playing",
+			pos: { "x": 0, "y": 0, "w": 0, "h":0 },
+			vol: 3 
+		}
+	);
+});
+
+app.get("/dtv/current-service/0x10", (req, res) => {
+	res.json(
+		{
+			streamContent: "0x6",
+			componentType: "0x03",
+			componentTag: "0x10",
+			ISO639languageCode: "por",
+			pid: 123,
+			state: "playing",
+			vol: 3 
+		}
+	);
+});
+
 
 app.get("/dtv/15.3.3/apps", (req,res) => {
 	res.json({ apps:[
